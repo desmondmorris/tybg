f8c20937cd66d59b71ba8146c8e17cac102306b1
Desmond Morris <hi@desmondmorris.com>
2013-09-25 00:48:23 -0400

    Always give thanks to the Based God

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,7 @@
     var html = '';
     html = $('body').html().replace(/god|yahweh|allah/ig,'Based God');
     html = html.replace(/jesus/ig, 'Lil B');
+    html = html.replace(/thank you/ig, 'thank you based god');
     $('body').html(html);
   });
 })(jQuery);
