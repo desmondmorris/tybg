d8199c74d292c0b1b8da0b0cefe256f76f9c1598
Desmond Morris <hi@desmondmorris.com>
2013-09-25 01:08:15 -0400

    Lets not repeat based

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,7 +4,8 @@
     var html = '';
     html = $('body').html().replace(/god|yahweh|allah/ig,'Based God');
     html = html.replace(/jesus/ig, 'Lil B');
-    html = html.replace(/thank you/ig, 'thank you based god');
+    html = html.replace(/thank you/ig, 'Thank You Based God');
+    html = html.replace(/based ?based god/ig, 'Based God');
     $('body').html(html);
   });
 })(jQuery);
