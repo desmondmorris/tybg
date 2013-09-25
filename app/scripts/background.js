a094a142c3f795bf70b4a2ff3de82d6832a4c083
Desmond Morris <hi@desmondmorris.com>
2013-09-25 01:14:26 -0400

    Fixes jshint errors

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,12 @@
 'use strict';
 (function ($) {
-  $(document).ready(function(){
-    var html = '';
-    html = $('body').html().replace(/god|yahweh|allah/ig,'Based God');
-    html = html.replace(/jesus/ig, 'Lil B');
-    html = html.replace(/thank you/ig, 'Thank You Based God');
-    html = html.replace(/based ?based god/ig, 'Based God');
-    $('body').html(html);
-  });
+    $(document).ready(function(){
+        var html = '';
+        html = $('body').html().replace(/god|yahweh|allah/ig,'Based God');
+        html = html.replace(/jesus/ig, 'Lil B');
+        html = html.replace(/thank you/ig, 'Thank You Based God');
+        html = html.replace(/based ?based god/ig, 'Based God');
+        $('body').html(html);
+    });
 })(jQuery);
 
